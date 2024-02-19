d79bca8db59f4b05ca8ccd6cbc5c069059e2b065
xchxn <seokregi@gmail.com>
2024-02-19 14:47:32 +0900

    mkdir to develope

--- comoback/src/app.module.ts
+++ comoback/src/app.module.ts
@@ -1,10 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './auth/auth.module';
+import { Database } from './database/database';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'password',
+      database: 'irproject',
+      entities: [],
+      synchronize: true,
+      logging: true,
+    }),
+    AuthModule,
+    DatabaseModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, Database],
 })
 export class AppModule {}
