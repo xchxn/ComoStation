8fa3e8603711f566df63ea824868f9a342869bdc
xchxn <seokregi@gmail.com>
2024-03-02 11:55:54 +0900

    fix the table

--- comofront/src/app/Board/page.tsx
+++ comofront/src/app/Board/page.tsx
@@ -1,23 +1,50 @@
 import Headers from "../component/layout/header";
+
+interface Post {
+  post_number: number;
+  title: string;
+  content: string;
+  writer: string;
+}
+
 async function getPosts() {
   'use server';
-  const res = await fetch(`http://localhost:3001/posting/getPosts`);
+  const res = await fetch(`http://localhost:3001/posting/getPosts`,{ next: { revalidate: 60 } });
 
   if (!res.ok) {
     throw new Error("fail fetch data");
   }
-
   return res.json();
 }
 
 export default async function Posting() {
-    const data = await getPosts()
+    const data = await getPosts();
+    console.log(data);
     //테이블처리
     return (
         <>
         <Headers />
         <div className="text-black bg-white">
-            { data.map((data:any) => <li key={data.post_number}> {data.title}</li>)}
+        <table>
+          <thead>
+            <tr>
+              <th>Post Number</th>
+              <th>Title</th>
+              <th>Content</th>
+              <th>Writer</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((post : Post) => (
+              <tr key={post.post_number}>
+                <td>{post.post_number}</td>
+                <td>{post.title}</td>
+                <td>{post.content}</td>
+                <td>{post.writer}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
         </div>
     </>
   )
