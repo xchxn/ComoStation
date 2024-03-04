5d8a01a01959b98f3fad384108c9a146908f8484
xchxn <seokregi@gmail.com>
2024-03-04 17:16:13 +0900

    change client component to use server

--- comofront/src/app/Login/page.tsx
+++ comofront/src/app/Login/page.tsx
@@ -1,11 +1,98 @@
-'use server'
-import LoginComponent from '../component/login';
+import Header from "../component/layout/header";
 import { cookies } from 'next/headers'
 
-export default async function Page(){
+async function Login(formData: any): Promise<any> {
+  "use server";
+  const rawFormData = {
+    id: formData.get("id"),
+    password: formData.get("password"),
+  };
+
+  console.log(rawFormData);
+
+  const requestOptions = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(rawFormData),
+  };
+
+  const res = await fetch(`http://localhost:3001/auth/login`, requestOptions);
+  // 서버에서의 응답을 JSON 형식으로 파싱
+  const data = await res.json();
+
+  // 사용자가 존재하는 경우
+  if (data) {
+    console.log("로그인 성공.");
+    //로그인 성공 시, 메인페이지로 리디렉션 요망.
+  } else {
+    // 사용자가 존재하지 않는 경우
+    console.log("로그인 실패.");
+  }
+}
+async function create() {
+  cookies().set('name', 'lee')
+  // or
+  cookies().set('name', 'lee', { secure: true })
+  // or
+  cookies().set({
+    name: 'name',
+    value: 'lee',
+    httpOnly: true,
+    path: '/',
+  })
+}
+
+export default function Page() {
   return (
-    <div>
-      <LoginComponent login></LoginComponent>
-    </div>
+    <>
+      <Header />
+      <ul className="flex justify-center">
+        <li className="grid box-content h-128 w-80 bg-slate-50 mx-2 rounded-lg my-4 justify-items-center overflow-hidden place-items-start p-6 py-8 sm:p-8 lg:p-12">
+          <h1 className="my-6 text-4xl text-black">🛰ComoStation</h1>
+          <form action={Login}>
+            <div>
+              <input
+                className="mx-2 my-1 px-2 py-1 border-2 border-solid rounded-lg text-black"
+                type="id"
+                id="id"
+                name="id"
+                placeholder="아이디"
+              />
+            </div>
+            <div>
+              <input
+                className="mx-2 my-1 px-2 py-1 border-2 border-solid rounded-lg text-black"
+                type="password"
+                id="password"
+                name="password"
+                placeholder="비밀번호"
+              />
+            </div>
+            <div>
+              <button
+                className="mx-2 my-1 border-2 border-solid rounded-md bg-blue-700 px-4 py-1"
+                type="submit"
+              >
+                Login
+              </button>
+              <button
+                className="mx-2 my-1 border-2 border-solid rounded-md bg-blue-700 px-4 py-1"
+                type="button"
+                // onClick={() => router.push("/SignUp")}
+              >
+                Sign Up
+              </button>
+            </div>
+            <div>
+              <p className="text-gray-400 text-sm">
+                If you don`t have an id, sign up
+              </p>
+            </div>
+          </form>
+        </li>
+      </ul>
+    </>
   );
 }
--- comofront/src/app/SignUp/page.tsx
+++ comofront/src/app/SignUp/page.tsx
@@ -1,9 +1,170 @@
-import Auth from '../component/auth';
+import LoginComponent from "../component/login";
+import { cookies } from "next/headers";
+import Header from "../component/layout/header";
 
-export default function Page(){
+async function signUp(formData: FormData): Promise<any> {
+  "use server";
+  const rawFormData = {
+    id: formData.get("id"),
+    password: formData.get("password"),
+    nickname: formData.get("nickname"),
+  };
+
+  console.log(rawFormData);
+
+  const requestOptions = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(rawFormData),
+  };
+
+  const res = await fetch(`http://localhost:3001/auth/signup`, requestOptions);
+
+  if (!res.ok) {
+    throw new Error("fail fetch data");
+  }
+  else console.log("회원가입 성공");
+  //로그인 페이지로 리디렉션 로직 추가 요망
+  return res.json();
+}
+
+async function IdCheck(formData: FormData): Promise<any> {
+  "use server";
+  const rawFormData = {
+    id: formData.get("id"),
+  };
+
+  console.log(rawFormData);
+
+  const requestOptions = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(rawFormData),
+  };
+
+  const res = await fetch(`http://localhost:3001/auth/idvalidcheck`, requestOptions);
+
+  if (!res.ok) {
+    throw new Error("fail fetch data");
+  }
+
+  const data = await res.json();
+  // 사용자가 존재하는 경우
+  if (data) {
+    console.log("동일한 아이디가 존재합니다.");
+  } else { // 사용자가 존재하지 않는 경우
+    console.log("사용가능한 아이디입니다.");
+  }
+}
+async function nicknameCheck(formData: FormData): Promise<any> {
+  "use server";
+  const rawFormData = {
+    nickname: formData.get("nickname"),
+  };
+
+  console.log(rawFormData);
+
+  const requestOptions = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(rawFormData),
+  };
+
+  const res = await fetch(`http://localhost:3001/auth/nicknamevalidcheck`, requestOptions);
+
+  if (!res.ok) {
+    throw new Error("fail fetch data");
+  }
+
+  const data = await res.json();
+  // 사용자가 존재하는 경우
+  if (data) {
+    console.log("동일한 닉네임이 존재합니다.");
+  } else { // 사용자가 존재하지 않는 경우
+    console.log("사용가능한 닉네임입니다.");
+  }
+}
+
+export default async function Page() {
   return (
     <div>
-      <Auth auth></Auth>
+      <Header />
+      <ul className="flex justify-center">
+        <li className="grid box-content h-128 w-80 bg-slate-50 mx-2 rounded-lg my-4 justify-items-center overflow-hidden place-items-start p-6 py-8 sm:p-8 lg:p-12">
+          <h1 className="my-6 text-4xl text-black">🛰ComoStation</h1>
+          <form action={signUp}>
+            <div>
+              <input
+                className="mx-2 my-1 px-2 py-1 border-2 border-solid rounded-lg text-black"
+                type="text"
+                id="id"
+                name="id"
+                placeholder="사용할 아이디"
+              />
+              <div>
+                <button
+                  id="idDupCheck"
+                  className="mx-2 my-1 border-2 border-solid rounded-md bg-blue-700 px-4 py-1"
+                  formAction={IdCheck}
+                >
+                  Check for duplication
+                </button>
+              </div>
+            </div>
+            <div>
+              <input
+                className="mx-2 my-1 px-2 py-1 border-2 border-solid rounded-lg text-black"
+                type="name"
+                id="nickname"
+                name="nickname"
+                placeholder="사용할 닉네임"
+              />
+            </div>
+            <div>
+              <button
+                id="ninknameDupCheck"
+                className="mx-2 my-1 border-2 border-solid rounded-md bg-blue-700 px-4 py-1"
+                formAction={nicknameCheck}
+              >
+                Check for duplication
+              </button>
+            </div>
+            <div>
+              <input
+                className="mx-2 my-1 px-2 py-1 border-2 border-solid rounded-lg text-black"
+                type="password"
+                id="password"
+                name="password"
+                placeholder="비밀번호"
+              />
+            </div>
+            <div>
+              <input
+                className="mx-2 my-1 px-2 py-1 border-2 border-solid rounded-lg text-black"
+                type="password"
+                id="pwCheck"
+                name="pwCheck"
+                placeholder="비밀번호 확인"
+              />
+            </div>
+            <div>
+              <button
+                id="signUp"
+                className="mx-2 my-1 border-2 border-solid rounded-md bg-blue-700 px-4 py-1"
+                type="submit"
+              >
+                Sign up
+              </button>
+            </div>
+          </form>
+        </li>
+      </ul>
     </div>
   );
 }
